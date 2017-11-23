8e6c25871a7c36a2268275555c942989b358c10d
user1 <user1@user1s-MacBook-Pro.local>
2017-11-23 17:09:15 +1100

    Text align for response

--- src/App.js
+++ src/App.js
@@ -5,33 +5,36 @@ import { questions } from './Data'
 
 class App extends React.Component {
   state = {
-    currentQ: questions,
     prevQ:[],
+    currentQ: questions,
     answer:[]
   }
-  nextQ = (e) =>{
-    console.log(e)
-    // this.setState({ prevQ: [item, ...this.state.currentQ] });
+  nextQ = (e, item) =>{
+    console.log(item)
+    this.setState({ prevQ: [item, ...this.state.prevQ] });
   }
   render() {
     const { currentQ, prevQ } = this.state
+    const numQ = Object.keys(prevQ).length
     return (
       <div>
-        {prevQ.map((x, indx) => {
-          return (
-            <div key={indx}> {x.message}<br /><br /></div>
-          );
-        })}
-        {currentQ.map((x, indx) => {
-          return (
-            <span>
-            <div key={indx}> {x.message}<br/><br/></div>
-            <button onClick={this.nextQ(x)}>send</button>
-            </span>
-          );
-        })[0]}
-        <input type="text" />
-        
+      #{numQ}
+        <br/>
+          {prevQ.map((x, indx) => {
+            return (
+              <div style={{textAlign:'right'}} key={indx}> {x.message}<br /><br /></div>
+            );
+          })}
+          {currentQ.map((x, indx) => {
+            return (
+              <span>
+              <div key={indx}> {x.message}<br/><br/></div>
+                <div style={{position:'absolute', bottom:'50%'}}><input type="text" />
+                  <button onClick={e => this.nextQ(e, x)}>send</button>
+                </div>
+              </span>
+            );
+          })[0]}
       </div>
     )
   }
