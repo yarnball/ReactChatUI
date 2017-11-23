6c7002ea65122f43ea3d783b5603964c4f33ad2c
user1 <user1@user1s-MacBook-Pro.local>
2017-11-23 17:19:40 +1100

    Gets previous value for answer

--- src/App.js
+++ src/App.js
@@ -5,31 +5,44 @@ import { questions } from './Data'
 
 class App extends React.Component {
   state = {
-    prevQ:[],
     currentQ: questions,
-    answer:[]
+    prevQ:[],
+    answer:[],
+    currentAns:{}
   }
   nextQ = (e, item) =>{
-    console.log(item)
+    console.log(e.target.value, item)
     this.setState({ prevQ: [item, ...this.state.prevQ] });
+    this.setState({ answer: [this.state.currentAns, ...this.state.answer] });
+  }
+  onChange = (e) =>{
+    console.log('e', e.target.value)
+    const ans = {name: e.target.value}
+    this.setState({ currentAns: ans });
   }
   render() {
-    const { currentQ, prevQ } = this.state
+    const { currentQ, prevQ, answer } = this.state
     const numQ = Object.keys(prevQ).length
     return (
       <div>
       #{numQ}
         <br/>
-          {prevQ.map((x, indx) => {
+          {answer.map((x, indx) => {
+            return (
+              <div style={{textAlign:'right'}} key={indx}> {x.name}<br /><br /></div>
+            );
+          })}
+{/*          {prevQ.map((x, indx) => {
             return (
               <div style={{textAlign:'right'}} key={indx}> {x.message}<br /><br /></div>
             );
-          })}
+          })}*/}
           {currentQ.map((x, indx) => {
             return (
               <span>
               <div key={indx}> {x.message}<br/><br/></div>
-                <div style={{position:'absolute', bottom:'50%'}}><input type="text" />
+                <div style={{position:'absolute', bottom:'50%'}}>
+                  <input onChange={this.onChange} type="text" />
                   <button onClick={e => this.nextQ(e, x)}>send</button>
                 </div>
               </span>
