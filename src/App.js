aa48112765153178618daf7c7b8baa9951329669
Yarnball <yconomos@gmail.com>
2018-11-21 11:48:27 +1100

    ref(lint): pretty format code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import { 
-  // randomLatin, 
-  carChoose 
-} from './Query/GetQuestions'
-import './App.css'
-import jsonLogic from 'json-logic-js'
-import dotty from 'dotty'
+import React from "react";
+import {
+  // randomLatin,
+  carChoose
+} from "./Query/GetQuestions";
+import "./App.css";
+import jsonLogic from "json-logic-js";
+import dotty from "dotty";
 
 class App extends React.Component {
   state = {
-    currentAns: '',
+    currentAns: "",
     isTyping: false,
     messages: [
-          {type:'q', text: 'Tell me a car band, I will tell you a model'},
+      { type: "q", text: "Tell me a car band, I will tell you a model" }
     ]
-  }
-  testPost = (e) =>{
+  };
+  testPost = e => {
     return fetch(
-          // post deleted item to archive DB
-           'http://localhost:3005/quotes', {
-           method: 'POST',
-           // mode: 'no-cors',
-           headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-           },
-           body: JSON.stringify({test:'genius'})
-           }).catch((err) => console.error(err)).then(() => console.log('allG'))
-  }
+      // post deleted item to archive DB
+      "http://localhost:3005/quotes",
+      {
+        method: "POST",
+        // mode: 'no-cors',
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ test: "genius" })
+      }
+    )
+      .catch(err => console.error(err))
+      .then(() => console.log("allG"));
+  };
   scrollToBottom = () => {
     this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     // dummy div created
-  }
+  };
 
   componentDidMount() {
     this.scrollToBottom();
@@ -40,77 +44,108 @@ class App extends React.Component {
   componentDidUpdate() {
     this.scrollToBottom();
   }
-  onChange = (e) =>{
-    this.setState({ currentAns: e.target.value  })
-  }
-  send = e =>{
-    e.preventDefault()
-    const { currentAns, messages } = this.state
-    const ans = {type: 'a', text: currentAns}
-    this.setState({ isTyping: true })
-    this.setState({ currentAns:'', messages: [...messages, ans] }, 
-      () => this.getNext(currentAns)
-    )
-  }
+  onChange = e => {
+    this.setState({ currentAns: e.target.value });
+  };
+  send = e => {
+    e.preventDefault();
+    const { currentAns, messages } = this.state;
+    const ans = { type: "a", text: currentAns };
+    this.setState({ isTyping: true });
+    this.setState({ currentAns: "", messages: [...messages, ans] }, () =>
+      this.getNext(currentAns)
+    );
+  };
 
-  getNext = currentAns =>{
-    const { messages } = this.state
-    let nextQ = {}
+  getNext = currentAns => {
+    const { messages } = this.state;
+    let nextQ = {};
     // nextQ = foodChoice(currentAns)
     // console.log("nex", nextQ)
     // ONLY use this on promise- it waits for the endpoint to resolve
     carChoose(currentAns)
-    .then((res) => {
-      nextQ = {type: 'q', text: res}
-    })
-    .then(() => this.setState({ isTyping: false, messages:  [...messages, nextQ] }))
+      .then(res => {
+        nextQ = { type: "q", text: res };
+      })
+      .then(() =>
+        this.setState({ isTyping: false, messages: [...messages, nextQ] })
+      );
     // const randomWait = Math.floor((Math.random() * 2200) + 100)
     // setTimeout(() => {
     //     this.setState({ isTyping: false, messages:  [...messages, nextQ] }),
     //     console.log('ne', nextQ)
     //   }, randomWait)
-  }
+  };
   testLogic = () => {
-    const cakeData = [{ "temp" : 100, "pies" : [{ "filling" : "apple","score":12 }, { "filling" : "cherry","score":12 }]}, { "temp" : 90, "pies" : [{ "filling" : "lemon","score":12 }, { "filling" : "orange","score":12 }]}]
+    const cakeData = [
+      {
+        temp: 100,
+        pies: [
+          { filling: "apple", score: 12 },
+          { filling: "cherry", score: 12 }
+        ]
+      },
+      {
+        temp: 90,
+        pies: [
+          { filling: "lemon", score: 12 },
+          { filling: "orange", score: 12 }
+        ]
+      }
+    ];
     return cakeData.map((itm, indx) => {
-       jsonLogic.add_operation('var_search', function(key){
-          return dotty.search(this, key); //In custom operations "this" is bound to data
-       });
-       var rules = { "and" : [
-          {"<" : [ { "var" : "temp" }, 110 ]},
-          {"in" : ["cherry", { "var_search" : "pies.*.filling" } ] },
+      jsonLogic.add_operation("var_search", function(key) {
+        return dotty.search(this, key); //In custom operations "this" is bound to data
+      });
+      var rules = {
+        and: [
+          { "<": [{ var: "temp" }, 110] },
+          { in: ["cherry", { var_search: "pies.*.filling" }] }
           // {"in" : [10, { ">" : "pies.*.score" } ]}
-        ] };
-        return jsonLogic.apply(rules, itm) ? itm : null;
-    })
-  }
+        ]
+      };
+      return jsonLogic.apply(rules, itm) ? itm : null;
+    });
+  };
 
   render() {
-    const { messages, currentAns, isTyping } = this.state
-    console.log('logic', this.testLogic())
+    const { messages, currentAns, isTyping } = this.state;
+    console.log("logic", this.testLogic());
 
     return (
       <div>
-      <header><span className="left">Messages</span><b style={{color:'black'}}>CarSelect</b><span className="right">Contact</span></header>
-<div className="messages-wrapper">
+        <header>
+          <span className="left">Messages</span>
+          <b style={{ color: "black" }}>CarSelect</b>
+          <span className="right">Contact</span>
+        </header>
+        <div className="messages-wrapper">
           {messages.map((x, indx) => {
             return (
               <span className="messageBody" key={indx}>
-              {x.type === 'q' && <div className="message from" > {x.text}</div>}
-              {x.type === 'a' && <div className="message to" > {x.text} </div>}
+                {x.type === "q" && (
+                  <div className="message from"> {x.text}</div>
+                )}
+                {x.type === "a" && <div className="message to"> {x.text} </div>}
 
-              <div style={{ float:"left", clear: "both" }} ref={(el) => { this.messagesEnd = el; }}></div>
-              
+                <div
+                  style={{ float: "left", clear: "both" }}
+                  ref={el => {
+                    this.messagesEnd = el;
+                  }}
+                />
               </span>
             );
           })}
           <button onClick={this.testPost}> test</button>
-          {isTyping && <div className="message from" > . . . </div>}
-          </div>
+          {isTyping && <div className="message from"> . . . </div>}
+        </div>
 
-          <form onSubmit={this.send}><input onChange={this.onChange} value={currentAns} type="text" /></form>
+        <form onSubmit={this.send}>
+          <input onChange={this.onChange} value={currentAns} type="text" />
+        </form>
       </div>
-    )
+    );
   }
 }
 
--- src/Query/GetQuestions.js
+++ src/Query/GetQuestions.js
@@ -1,36 +1,35 @@
-import { questions } from './Data'
+import { questions } from "./Data";
 
-export const carChoose = e =>{
-  const origQ = e
-    return fetch('https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/' + e.replace(/[^\w\s]|_/g, "").replace(/\W+/g, " ") + '?format=json',
-      // the replace removes all punctuation and spaces
-      )
-      .then((response) => {
-        if(response.ok) {
-            return response.json()
-        } else {
-            throw new Error('Server response wasn\'t OK');
-        }
-      })
-      .then((json) => {
-        if (json.Count > 0) {
-          const RandomNum = Math.floor(Math.random() * json.Count-1) + 1
-          // Gets the total count of items, picks a random number
-          console.log('all res', RandomNum)
-          return json.Results[RandomNum].Model_Name
-        }
-        else {
-          return origQ + `? Are you sure thats a car?`
-        }
-        
-        
-        
-      })
-}
+export const carChoose = e => {
+  const origQ = e;
+  return fetch(
+    "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/" +
+      e.replace(/[^\w\s]|_/g, "").replace(/\W+/g, " ") +
+      "?format=json"
+    // the replace removes all punctuation and spaces
+  )
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Server response wasn't OK");
+      }
+    })
+    .then(json => {
+      if (json.Count > 0) {
+        const RandomNum = Math.floor(Math.random() * json.Count - 1) + 1;
+        // Gets the total count of items, picks a random number
+        console.log("all res", RandomNum);
+        return json.Results[RandomNum].Model_Name;
+      } else {
+        return origQ + `? Are you sure thats a car?`;
+      }
+    });
+};
 
-export const randomLatin = e =>{
-    const randomNum = Math.floor((Math.random() * 100) + 1)
-    const randomQ = questions.filter(e=> e.id === randomNum).pop()
-    const nextQ = {type:'q', text:randomQ.text}
-    return nextQ
-}
+export const randomLatin = e => {
+  const randomNum = Math.floor(Math.random() * 100 + 1);
+  const randomQ = questions.filter(e => e.id === randomNum).pop();
+  const nextQ = { type: "q", text: randomQ.text };
+  return nextQ;
+};
